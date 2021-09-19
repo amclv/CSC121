7fe2e9a4692fe43c821e508bcd9d63a05eae2143
amclv <toscleveland@gmail.com>
2021-09-19 03:05:23 -0500

    Guessed what numbers were gonna be displayed

---
+++ Chapter 1/Prepost/Prepost.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int x;
+    x = 25; // 25
+    x = x + 5; // new x = old x + 5 // 30
+    x++; // x = x + 1; post increment // 31
+    
+    cout << x << endl; // Guess 31
+    cout << x++ << endl; // Guess 32
+    cout << ++x << endl; // Guess 33
+    cout << --x << endl; // Guess // Pre increment 32
+    cout << x-- << endl; // Guess 31
+    cout << x << endl; // Guess 31
+    // x = 100 - x++; // 
+    // cout << x << endl;
+
+    // float y = x / 3.0; // Watch out for the operand datatypes
+    // int i = 3;
+    // float y = float(x) / i;
+    // cout << y << endl;
+}
