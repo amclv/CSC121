e42ef9daad189ef0cdb05bbb51b51b596eb4a219
amclv <toscleveland@gmail.com>
2021-09-28 23:53:15 -0500

    Added Prepost

--- Chapter 1/Prepost/Prepost.cpp
+++ Chapter 1/Prepost/Prepost.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 int main() 
@@ -8,18 +7,20 @@ int main()
     x = 25; // 25
     x = x + 5; // new x = old x + 5 // 30
     x++; // x = x + 1; post increment // 31
-    
+
     cout << x << endl; // Guess 31
     cout << x++ << endl; // Guess 32
     cout << ++x << endl; // Guess 33
-    cout << --x << endl; // Guess // Pre increment 32
+    cout << --x << endl; // Guess 32 // Pre increment
     cout << x-- << endl; // Guess 31
     cout << x << endl; // Guess 31
-    // x = 100 - x++; // 
-    // cout << x << endl;
+    x = 100 - x++; // 100 - x
+    cout << x << endl;
 
-    // float y = x / 3.0; // Watch out for the operand datatypes
-    // int i = 3;
-    // float y = float(x) / i;
-    // cout << y << endl;
+    float y = x / 3.0; // Watch out for the operand datatypes
+    cout << y << endl; //
+    int i = 3;
+    y = float(x) / i; // static casting
+    cout << y << endl; //
+    return 0;
 }
