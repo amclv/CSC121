b83f8aa97dfecc2200b56aed8d3d5b0bc4815962
amclv <toscleveland@gmail.com>
2021-10-12 19:29:46 -0500

    Finished Week 3 CaseSwitch - Finished some minor notes Loops - Added Loops homework Loops.txt - Added output of Loops file

--- CSC121/CSC121/Chapter 3/CaseSwitch.cpp
+++ CSC121/CSC121/Chapter 3/CaseSwitch.cpp
@@ -15,67 +15,67 @@
 
 using namespace std;
 
-int main()
-{
-    int score = 0; // Declare an integer variable
-    cout << "\nPlease enter a value for score: ";
-    cin >> score; // Get the score
-    cout << "\n Here is the value you entered " << score << endl; //
-    
-    // Prepare the code for menu
-    cout << "\n What would you like to do with this score? : \n";
-    cout << "1. Perfect Score Check \n";
-    cout << "2. Even/Odd Check \n";
-    cout << "3. Letter Grade Check \n";
-    cout << "4. Conditional operator used for Even/Odd Check \n";
-    cout << "9. Quit\n";
-    
-    int menu = 0;
-    cin >> menu; // Get the menu item
-    
-    // Code the case switch statement to respond to the menu
-    switch (menu)
-    {
-        case 1: // Perfect Score
-        {
-            if (score == 100)
-            {
-                cout << "\nPerfect Score!\n";
-                cout << "Bravo! Great!\n";
-            } // End of if
-            break;
-        } // End of case 1
-        case 2: // Even/Odd Check
-        {
-            if ((score % 2) == 0)
-                cout << "\n Score is Even!\n"; // True
-            else
-                cout << "\n Score is Odd!\n"; // False
-            break;
-        } // End of case 2
-        case 3:
-        {
-            if (score >= 90) // A
-                cout << "\n Letter Grade is A\n";
-            else if (score >= 80) // B
-                cout << "\n Letter Grade is B\n";
-            else if (score >= 70) // C
-                cout << "\n Letter Grade is C\n";
-            else if (score >= 60) // D
-                cout << "\n Letter Grade is D\n";
-            else // F
-                cout << "\n Letter Grade is F, please try harder!\n";
-            break;
-        } // End of case 3
-        case 4: // Conditional operator is short for of if then else
-        { // syntax is ? true part : false part
-            ((score % 2) == 0)? cout << "\n Score is Even!\n" : cout << "\n Score is Odd!\n";
-            break;
-        } // End of case 4
-        case 9:
-        {
-            cout << endl;
-        } // End of case 9
-    } // End of Switch
-    return 0;
-}
+//int main()
+//{
+//    int score = 0; // Declare an integer variable
+//    cout << "\nPlease enter a value for score: ";
+//    cin >> score; // Get the score
+//    cout << "\n Here is the value you entered " << score << endl; //
+//
+//    // Prepare the code for menu
+//    cout << "\n What would you like to do with this score? : \n";
+//    cout << "1. Perfect Score Check \n";
+//    cout << "2. Even/Odd Check \n";
+//    cout << "3. Letter Grade Check \n";
+//    cout << "4. Conditional operator used for Even/Odd Check \n";
+//    cout << "9. Quit\n";
+//
+//    int menu = 0;
+//    cin >> menu; // Get the menu item
+//
+//    // Code the case switch statement to respond to the menu
+//    switch (menu)
+//    {
+//        case 1: // Perfect Score
+//        {
+//            if (score == 100)
+//            {
+//                cout << "\nPerfect Score!\n";
+//                cout << "Bravo! Great!\n";
+//            } // End of if
+//            break;
+//        } // End of case 1
+//        case 2: // Even/Odd Check
+//        {
+//            if ((score % 2) == 0)
+//                cout << "\n Score is Even!\n"; // True
+//            else
+//                cout << "\n Score is Odd!\n"; // False
+//            break;
+//        } // End of case 2
+//        case 3:
+//        {
+//            if (score >= 90) // A
+//                cout << "\n Letter Grade is A\n";
+//            else if (score >= 80) // B
+//                cout << "\n Letter Grade is B\n";
+//            else if (score >= 70) // C
+//                cout << "\n Letter Grade is C\n";
+//            else if (score >= 60) // D
+//                cout << "\n Letter Grade is D\n";
+//            else // F
+//                cout << "\n Letter Grade is F, please try harder!\n";
+//            break;
+//        } // End of case 3
+//        case 4: // Conditional operator is short for of if then else
+//        { // syntax is ? true part : false part
+//            ((score % 2) == 0)? cout << "\n Score is Even!\n" : cout << "\n Score is Odd!\n";
+//            break;
+//        } // End of case 4
+//        case 9:
+//        {
+//            cout << endl;
+//        } // End of case 9
+//    } // End of Switch
+//    return 0;
+//}
--- CSC121/CSC121/Chapter 3/Loops.cpp
+++ CSC121/CSC121/Chapter 3/Loops.cpp
@@ -4,5 +4,41 @@
 //
 //  Created by Aaron Cleveland on 10/12/21.
 //
+/*
+ Three types of loops. "while", "do while", and "for"
+ */
 
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+//int main()
+//{
+//    
+//    // Print the values of CV 1 - 10.
+//    // i is CONTROL variable
+//    for(int i = 1; i <= 10; i++) // is a FIXED, PRE-TEST loop #1
+//    { // Block of code
+//        cout << "Value of i is: " << i << endl;
+//    }
+//    cout << endl; // Break in output
+//    
+//    int j = 1; // #1
+//    while (j <= 10) // Is a VARIABLE, PRE-TEST loop #2
+//    {
+//        cout << "Value of j is: " << j << endl;
+//        j++; // #3
+//    }
+//    cout << endl; // Break in output
+//    
+//    int k = 1; // #1
+//    do // is VARIABLE, POST-TEST loop #3
+//    {
+//        cout << "Value of k is " << k << endl;
+//        k++; // #3
+//    } while (k <= 10); // Don't forget the semicolon after the while and its condition. #2
+//    cout << endl; // Break in output
+//    
+//    return 0;
+//}
