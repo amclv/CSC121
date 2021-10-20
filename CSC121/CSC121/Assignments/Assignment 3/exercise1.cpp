0ac5e6357567fc1c86aa4340fce7e084708dc215
amclv <toscleveland@gmail.com>
2021-10-20 00:13:14 -0500

    Added assignment 3.

--- CSC121/CSC121/Assignments/Assignment 3/exercise1.cpp
+++ CSC121/CSC121/Assignments/Assignment 3/exercise1.cpp
@@ -8,8 +8,39 @@
 #include <stdio.h>
 #include <iostream>
 
+using namespace std;
+
+const int DEFAULT_YEAR = 1582;
+
 int main()
 {
-    
+    int year;
+
+    cout << "Please enter a year: ";
+    cin >> year;
+
+    /*
+     check if the year is before 1582 or after 1582.
+     if its less then 1582 then there is no rules its just divisible by 4
+     if its greater then 1582 then we check the exceptions.
+        its a leap year if its divisible by 4 or 400 but not for 100
+     */
+
+    if (year < DEFAULT_YEAR)
+    {
+        ((year % 4) == 0) ? cout << year << " is a leap year." : cout << year << " is not a leap year";
+    }
+    else
+    {
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+        {
+            cout << year << " is a leap year." << endl;
+        }
+        else
+        {
+            cout << year << " is not a leap year." << endl;
+        }
+    }
+
     return 0;
 }
--- CSC121/CSC121/Assignments/Assignment 3/exercise2.cpp
+++ CSC121/CSC121/Assignments/Assignment 3/exercise2.cpp
@@ -6,3 +6,35 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number; // Users input
+    int digitValues[] = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 }; // This is an integer array that uses all of our digit values
+    string symbolValues[] = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" }; // This is a string array that uses all of our roman symbols
+    string result;
+    
+    cout << "Enter the whole number less than 4,000 you wish to convert: ";
+    cin >> number;
+    
+    for (int i = 12; i >= 0; --i) // A loop to check if the number that is inputed is greater than the constant
+    {
+        if (number >= digitValues[i])
+        {
+            int divide = number / digitValues[i]; // Finding the number of times the number that we ask gets divided by the constant
+            
+            for (int x = 0; x < divide; x++)
+            {
+                result += symbolValues[i]; // Loops through and appends the roman digits to the result string above.
+            }
+            number = number % digitValues[i]; // Finds the remaining numbers
+        }
+    }
+    cout << "The Roman nummeral is " << result << endl; // Prints the answer out to the user.
+    
+    
+    return 0;
+}
