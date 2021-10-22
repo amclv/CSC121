32ed6a26e6ad878b2e992bae7dceae0e07a881b3
amclv <toscleveland@gmail.com>
2021-10-22 00:46:36 -0500

    Added assignment 4 and output

--- CSC121/CSC121/Week 4_5/function4.cpp
+++ CSC121/CSC121/Week 4_5/function4.cpp
@@ -4,13 +4,40 @@
 //
 //  Created by Aaron Cleveland on 10/22/21.
 //
+/*
+ TWO WAY COMMUNICATION
+ */
 
 #include <stdio.h>
 #include <iostream>
 
 using namespace std;
 
+/*
+ This is a value function with two integer parameters.
+ This function returns an integer value which is the product of the two numbers
+ that are passed as parameters.
+ */
+int Product(int, int);
+
 int main()
 {
+    int n1;
+    int n2;
+    
+    cout << "Please enter two integers: ";
+    cin >> n1 >> n2;
+    
+    // n1 and n2 are ACTUAL parameters(arugments) VALUES --> constant/variables/expression/fn call
+    cout << "The product of " << n1 << " and " << n2 << " is: " << Product(n1, n2) << endl;
+    
     return 0;
+}
+
+// VAR = VALUE;
+int Product(int n1, int n2)
+{
+    // int Product(int n1, int n2) // n1 and n2 are FORMAL parameters(parameters)VARIABLES
+    
+    return (n1 * n2);
 }
