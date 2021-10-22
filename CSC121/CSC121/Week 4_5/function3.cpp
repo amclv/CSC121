4dd5ddc9fbbf6c77d465bd28bfce2cf0deda6597
amclv <toscleveland@gmail.com>
2021-10-22 00:26:28 -0500

    Added function 3 assignment

---
+++ CSC121/CSC121/Week 4_5/function3.cpp
@@ -0,0 +1,37 @@
+//
+//  function3.cpp
+//  CSC121
+//
+//  Created by Aaron Cleveland on 10/22/21.
+//
+/*
+ One way communication out only VALUE RETURNING
+ */
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+ This is a value function with no parameters. The function returns a string datatype
+ */
+
+string Header(); // PROTOTYPE function with a string data type returned
+
+int main()
+{
+//    cout << Header() << endl;
+    string T = Header();
+    cout << T << endl;
+    
+//    Header(); // Value returning function CANT be called on a line by itself. // ONLY VOID CAN BE CALLED ON ITSELF.
+    
+    return 0;
+}
+
+string Header() // Function Header
+{
+    return "\n This is the value returned\n"; // Returning literal string
+}
