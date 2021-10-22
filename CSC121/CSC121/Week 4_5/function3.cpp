e2602cf9358ebdf578f21572a84683486ca453f4
Aaron Cleveland <toscleveland@gmail.com>
2021-10-22 00:46:52 -0500

    init assignment 4 (#4)
    * init assignment 4
    
    * Added assignment 4 and output

--- CSC121/CSC121/Week 4_5/function3.cpp
+++ CSC121/CSC121/Week 4_5/function3.cpp
@@ -18,20 +18,20 @@ using namespace std;
  This is a value function with no parameters. The function returns a string datatype
  */
 
-string Header(); // PROTOTYPE function with a string data type returned
-
-int main()
-{
-//    cout << Header() << endl;
-    string T = Header();
-    cout << T << endl;
-    
-//    Header(); // Value returning function CANT be called on a line by itself. // ONLY VOID CAN BE CALLED ON ITSELF.
-    
-    return 0;
-}
-
-string Header() // Function Header
-{
-    return "\n This is the value returned\n"; // Returning literal string
-}
+//string Header(); // PROTOTYPE function with a string data type returned
+//
+//int main()
+//{
+////    cout << Header() << endl;
+//    string T = Header();
+//    cout << T << endl;
+//    
+////    Header(); // Value returning function CANT be called on a line by itself. // ONLY VOID CAN BE CALLED ON ITSELF.
+//    
+//    return 0;
+//}
+//
+//string Header() // Function Header
+//{
+//    return "\n This is the value returned\n"; // Returning literal string
+//}
