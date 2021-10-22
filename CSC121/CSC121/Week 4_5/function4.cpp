2f3e77f5b4f58b8d9fc925dea55a288c783e941d
amclv <toscleveland@gmail.com>
2021-10-22 00:36:06 -0500

    init assignment 4

---
+++ CSC121/CSC121/Week 4_5/function4.cpp
@@ -0,0 +1,16 @@
+//
+//  function4.cpp
+//  CSC121
+//
+//  Created by Aaron Cleveland on 10/22/21.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
